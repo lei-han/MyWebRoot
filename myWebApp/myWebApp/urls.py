dc86e77f40632c269bcb6ec1beac67674f242472
lei-han <ihlei001@gmail.com>
2014-11-18 01:30:57 +0800

    trial@home

--- myWebApp/myWebApp/urls.py
+++ myWebApp/myWebApp/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from views import hello,now,futureHours,showName
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myWebApp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$',hello),
+    url(r'^now/$', now),
+    url(r'^future/(\d{1,2})/$', futureHours),
+    url(r'^nameis/([a-zA-Z]{1,10})/$', showName),
 )
